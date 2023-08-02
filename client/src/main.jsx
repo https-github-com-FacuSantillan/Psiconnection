28e2c4671ab2897779ff4a22ae1f96df5258dadb
lucasemanuel <gaitonlucas7@gmail.com>
2023-08-02 16:30:04 -0300

    Instalacion de dependecias listo para empezar a craer las routes y los componentes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//importmas browserRouter 
+import { BrowserRouter } from 'react-router-dom'
+//Imports para la utilizacion de redux
+import { store } from './Redux/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
