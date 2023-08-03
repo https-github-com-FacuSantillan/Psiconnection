b54e24e487e568e54af98a3226c2078b8740496c
delfinadelossantos <delfinadelossantos@gmail.com>
2023-08-03 19:34:39 -0300

    [feature] ruta getDetail

--- api/src/controllers/psicologosController.js
+++ api/src/controllers/psicologosController.js
@@ -1,35 +1,57 @@
-const { encrypt, compare } = require('../helpers/handleBcrypt.js')
+const { encrypt, compare } = require("../helpers/handleBcrypt.js");
+const { Psicologo } = require("../db.js");
+
+//Controlador para búsqueda por id
+const getDetailController = async (id) => {
+  const detail = await Psicologo.findByPk(id);
+  return detail;
+};
 
 // controlador de registro para crear psicologo http://localhost:3001/psiconection/registerPsicologo --- Psicologo
-const createUsuarioPsicologo = async ({ nombre, apellido, email, usuario, edad, password, pais, genero, licencia, disponibilidad, tarifa, especialidad, whatsAppUrl, telefono, foto, descripcion }) => {
-    const passwordHash = await encrypt(password)
-         PsicologoUser =  { 
-            nombre, 
-            apellido, 
-            email, 
-            usuario, 
-            edad, 
-            password: passwordHash, 
-            pais, 
-            genero, 
-            licencia, 
-            disponibilidad, 
-            tarifa, 
-            especialidad, 
-            whatsAppUrl, 
-            telefono, 
-            foto, 
-            descripcion 
-        };
-      
-        return {
-            Psicologo : PsicologoUser
-        }
-}
+const createUsuarioPsicologo = async ({
+  nombre,
+  apellido,
+  email,
+  usuario,
+  edad,
+  password,
+  pais,
+  genero,
+  licencia,
+  disponibilidad,
+  tarifa,
+  especialidad,
+  whatsAppUrl,
+  telefono,
+  foto,
+  descripcion,
+}) => {
+  const passwordHash = await encrypt(password);
+  PsicologoUser = {
+    nombre,
+    apellido,
+    email,
+    usuario,
+    edad,
+    password: passwordHash,
+    pais,
+    genero,
+    licencia,
+    disponibilidad,
+    tarifa,
+    especialidad,
+    whatsAppUrl,
+    telefono,
+    foto,
+    descripcion,
+  };
 
-
-
+  return {
+    Psicologo: PsicologoUser,
+  };
+};
 
 module.exports = {
-    createUsuarioPsicologo,
-}
+  createUsuarioPsicologo,
+  getDetailController,
+};
--- api/src/routes/psicologosRoutes.js
+++ api/src/routes/psicologosRoutes.js
@@ -1,25 +1,26 @@
-const { Router } = require('express');
-
+const { Router } = require("express");
 
 // handlers
-const { manejadorPrueba, registerHandler } = require('../handlers/psicologosHandlers.js');
-
+const {
+  manejadorPrueba,
+  registerHandler,
+  getDetailHandler,
+} = require("../handlers/psicologosHandlers.js");
 
 const psicologosRoutes = Router();
 
-
 // ruta de prueba http://localhost:3001/psiconection/prueba
-psicologosRoutes.get('/prueba', manejadorPrueba);
+psicologosRoutes.get("/prueba", manejadorPrueba);
 
+//Ruta de búsqueda por id
+psicologosRoutes.get("/:id", getDetailHandler);
 
 //! registro
 // ruta tipo post http://localhost:3001/psiconection/registerPsicologo --- Psicologo
-psicologosRoutes.post('/registerPsicologo', registerHandler)
+psicologosRoutes.post("/registerPsicologo", registerHandler);
 
-
-//! login 
+//! login
 //ruta tipo post http://localhost:3001/psiconection/login
-psicologosRoutes.post('/login')
-
+psicologosRoutes.post("/login");
 
 module.exports = psicologosRoutes;
