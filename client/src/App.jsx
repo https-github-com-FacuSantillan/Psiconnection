9153b0e8c851b6ceaebd13d97ad0ccacdd4c9d74
Facuusantillan <facusantillan1@gmail.com>
2023-08-03 16:09:59 -0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,18 +21,11 @@ function App() {
 
   return (
     <div>
-<<<<<<< HEAD
       <Routes>
+        {pathname !== '/' && <SideBar/>}
         <Route path='/' element={<Landing />}></Route>
         <Route path='/home' element={<Home />} />
 
-=======
-      {pathname !== '/' && <SideBar/>}
-
-      <Routes>
-
-      <Route path='/home' element={<Home/>} />
->>>>>>> 4b49cb660af7a7e21566018989efd7a9dc6e301c
 
       </Routes>
     </div>
