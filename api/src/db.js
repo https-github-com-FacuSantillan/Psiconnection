2d731e00898455a0f4f13851f543fd53954ae65d
Juan-M-Loaiza <juanmloaiz4@gmail.com>
2023-08-03 13:38:12 -0500

    conexión con BDD solucionada

--- api/src/db.js
+++ api/src/db.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 const {
     DB_USER, DB_PASSWORD, DB_HOST,
   } = process.env;
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/psiconnection`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/psiconnection?sslmode=require`, {
     logging: false,
     native: false,
     dialectOptions: {
@@ -15,6 +17,32 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
         }
       }
 });
+
+
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach(model => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+
+const { Psicologo, Usuario, Especialidad  } = sequelize.models;
+
+Psicologo.belongsToMany(Especialidad, { through: "PsicologoEspecialidad", timestamps: false })
+Especialidad.belongsToMany(Psicologo, { through: "PsicologoEspecialidad", timestamps: false })
+
 
 
 module.exports = {
