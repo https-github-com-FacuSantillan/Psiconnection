695877ba3781e30381c2bd5252ae6391293f9ef4
JotaLed <jonathanledezma09@gmail.com>
2023-08-04 15:43:17 -0300

    Comentario de prueba

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const server = require("./src/app.js");
 require("dotenv").config();
 const { PORT } = process.env;
-const { conn} = require('./src/db.js');
+const { conn } = require("./src/db.js");
 
-
+//Comentario de ejemplooooooooooo
 
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
