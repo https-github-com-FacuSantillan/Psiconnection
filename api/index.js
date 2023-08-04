5703a058aa9c9f54d81da26309a8e8c1de99e8c4
Juan-M-Loaiza <juanmloaiz4@gmail.com>
2023-08-04 13:40:36 -0500

    sync: force|true

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const server = require("./src/app.js");
 require("dotenv").config();
 const { PORT } = process.env;
 const { conn} = require('./src/db.js');
+const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env;
 
 
 
