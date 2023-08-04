05e75640ba0e61ffcfecdada4a9c694e1f874f8a
Juan-M-Loaiza <juanmloaiz4@gmail.com>
2023-08-04 13:42:50 -0500

    middleware multer

--- api/src/utils/upload.js
+++ api/src/utils/upload.js
@@ -1,25 +1,16 @@
-require('dotenv').config();
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('cloudinary');
+
 const multer = require('multer')
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
 });
 
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'uploads', // La carpeta en la que se guardarán las imágenes en Cloudinary
-      allowed_formats: ['jpg', 'jpeg', 'png'], // Formatos de archivo permitidos
-    },
-  });
 
-  const upload = multer({ storage: storage });
+  const upload = multer({storage: storage});
   
-  module.exports = upload 
+  module.exports = upload ;
 
 
 
