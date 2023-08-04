c4544f0873b881a06f7ad209ea3d2567d1117110
Juan-M-Loaiza <juanmloaiz4@gmail.com>
2023-08-04 13:45:03 -0500

    nueva ruta put actualizacion foto

--- api/src/routes/psicologosRoutes.js
+++ api/src/routes/psicologosRoutes.js
@@ -4,24 +4,33 @@ const upload = require('../utils/upload');
 
 
 // handlers
-const { manejadorPrueba, registerHandler } = require('../handlers/psicologosHandlers.js');
-
+const {
+  // manejadorPrueba,
+  registerHandler,
+  getDetailHandler,
+  subirFoto
+} = require("../handlers/psicologosHandlers.js");
 
 const psicologosRoutes = Router();
 
-
 // ruta de prueba http://localhost:3001/psiconection/prueba
-psicologosRoutes.get('/prueba', manejadorPrueba);
+// psicologosRoutes.get("/prueba", manejadorPrueba);
 
+// Ruta de búsqueda por id
+psicologosRoutes.get("/:id", getDetailHandler);
 
 //! registro
 // ruta tipo post http://localhost:3001/psiconection/registerPsicologo --- Psicologo
-psicologosRoutes.post('/registerPsicologo', upload.single('foto'), registerHandler)
+psicologosRoutes.post('/registerPsicologo', registerHandler);
 
 
-//! login 
+
+psicologosRoutes.put('/uploadFoto/:id',upload.single('foto'), subirFoto); 
+
+
+
+//! login
 //ruta tipo post http://localhost:3001/psiconection/login
-psicologosRoutes.post('/login')
-
+psicologosRoutes.post("/login");
 
 module.exports = psicologosRoutes;
