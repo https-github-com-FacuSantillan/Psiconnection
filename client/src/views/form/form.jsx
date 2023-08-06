ff7832cf7fd36f987570f31178855793f4654535
AnaMariaAlvaradoM <anaa_alvarado@hotmail.com>
2023-08-06 14:32:10 -0500

    avance2

--- client/src/views/form/form.jsx
+++ client/src/views/form/form.jsx
@@ -91,8 +91,8 @@ const Form = () => {
       {/* <h2>{formData.isLogin ? "" : ""}</h2> */}
       {!formData.tipoUsuario && !formData.isLogin && (
          <div>
-          <p1>¡Hola!</p1>
-          <p >Selecciona tu tipo de usuario:</p>
+          <p className="p1">¡Hola!</p>
+          <p className="p2">Selecciona tu tipo de usuario:</p>
          <div>
            <button onClick={() => handleUserTypeSelect("psicologo")}>Soy Psicólogo</button>
            <button onClick={() => handleUserTypeSelect("consultante")}>Soy Consultante</button>
@@ -137,9 +137,9 @@ const Form = () => {
               <button type="submit">Iniciar sesión</button>
               <p>
                 ¿No tienes cuenta? <hr />
-                  <p3 onClick={toggleLogin}>
+                  <p className="p2" onClick={toggleLogin}>
                     Registrarse
-                  </p3>
+                  </p>
               </p> 
             </>
           ) : (
