35eda8d273aebdbe558fd63d79431782d74bcb5f
Pinkfrost28 <macmacheta@gmail.com>
2023-08-05 22:21:51 -0500

    continuacion de paginado

--- client/src/views/home/home.jsx
+++ client/src/views/home/home.jsx
@@ -5,8 +5,21 @@ import CardsContainer from '../../components/CardsContainer/CardsContainer';
 import Sidebar from '../../components/sideBar/sideBar';
 import style from "./home.module.css"
 import Pagination from '../../components/Pagination/Pagination';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+  const ITEMS_PER_PAGE = 9;
+  const [items, setItems] = useState([])
+  const [currentPage, setCurrentPage] = useState(0)
+  const piscologos = useSelector(state => state.psychologists)
+  useEffect(() =>{
+    if(piscologos.length === 0){
+      //dispatch(getPsicologos())
+    }
+    const slicedPsic =  piscologos.slice(0, ITEMS_PER_PAGE) 
+  }, [])
+
     return (
     
         <div>
