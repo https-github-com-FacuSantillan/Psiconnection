5eb8be4041e4ede23fcd28796865604c5c60d115
JotaLed <jonathanledezma09@gmail.com>
2023-08-06 13:46:46 -0300

    borrado comentario de ejemplo

--- api/index.js
+++ api/index.js
@@ -4,8 +4,6 @@ const { PORT } = process.env;
 
 const { conn } = require("./src/db.js");
 
-//Comentario de ejemploooooooooooooooooooooooooooooo
-
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log("listening at port ", PORT);
